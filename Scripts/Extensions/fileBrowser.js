73005a17169089d6bc79db3557f9120a7786b5ed
Ayman Bouchareb <Ayman.Bouchareb@outlook.fr>
2022-06-28 18:38:51 +0100

    Build test for : 29/06/2022 (Atlantis in the Netherverse meeting)

--- Scripts/Extensions/fileBrowser.js
+++ Scripts/Extensions/fileBrowser.js
@@ -1,3 +1,7 @@
+FileType = {
+    Url : 1, Video : 2, Audio : 4, Image : 8, Object : 16, Text : 32, Presentation : 64, 
+}
+
 const GetLocalFile =  async (type) => {
     const processZip = async (zip) => {
         var promises = Object.keys(zip.files)
@@ -8,49 +12,77 @@ const GetLocalFile =  async (type) => {
     }
 
     
-    GetLocalFileProcess(async data => {
-        var zip = new JSZip();
-        var images = await zip.loadAsync(data).then(processZip);
-        var buildMessage = (body) => type + "|" + body;
-        var message = "";
-        if(type == 2) {
-            message = buildMessage(JSON.stringify({
-                Slides : images,
-                Index : 0
-            }));
-        } else {
-            console.log("not supported");
-            alert("import media of this type is not yet supported");
-            return;
+    GetLocalFileProcess(type, async (name, data) => {
+        var fileObject = {
+            Type : type,
+            Id   : 0,
+            Name : name,
+            Data : "",
+            Details : {
+                ZoneId   : 0,
+                SenderId : 0,
+                Epoch    : 0
+            }
+        };
+        switch (type) {
+            case FileType.Presentation:
+                var zip = new JSZip();
+                var images = await zip.loadAsync(data).then(processZip);
+                fileObject.Data = JSON.stringify({
+                    Slides : images,
+                    Index : 0
+                })
+                break;
+            default:
+                fileObject.Data = data.toString().split(',')[1] 
+                break;
         }
-        WebglInstance.SendMessage("GameManager", "HandleFile", message);
+        var param = URL.createObjectURL(new Blob([JSON.stringify(fileObject)], {type: "application/json"}));
+        WebglInstance.SendMessage("GameManager", "HandleFileData", param);
     });
 }
 
-const GetLocalFileProcess = async (callBack) => {
+const GetLocalFileProcess = async (type, callBack) => {
     var input = document.createElement('input');
+    var extensionFilter = "";
+    switch (type) {
+        case FileType.Text:
+            extensionFilter = ".txt"
+            break;
+        case FileType.Video:
+            extensionFilter = ".mp4"
+            break;
+        case FileType.Audio:
+            extensionFilter = ".mp3"
+            break;
+        case FileType.Image:
+            extensionFilter = ".jpg, .png"
+            break;
+        case FileType.Object:
+            extensionFilter = ".glb"
+            break;
+        case FileType.Presentation:
+            extensionFilter = ".zip"
+            break;
+        default:
+            return;
+    }
     input.type = 'file';
-    var byteData;
+    input.accept = extensionFilter;
     input.onchange = e => { 
         var file = e.target.files[0]; 
         var reader = new FileReader();
-        reader.readAsArrayBuffer(file);
+
+        if(type == 2 || type == 4 || type == 8 || type == 16 || type == 32){
+            reader.readAsDataURL(file);
+        } else {
+            reader.readAsArrayBuffer(file);
+        }
+        
         reader.onload = readerEvent => {
-            byteData = readerEvent.target.result;
-            callBack(byteData);
+            var byteData = readerEvent.target.result;
+            callBack(file.name ,byteData);
         }
     }
     input.click();
-}
-
-
-const handleFile = (fileData, metadata) => {
-    var file = JSON.parse(fileData);
-    // upload file to server and get url
-    var url = "https://dummy.url.for.file.upload";
-    // send([localUser], { Source   : handlers.Browser /* Browser */,
-    //                     Action   : actions.File,
-    //                     Body     : url,
-    //                     SenderId : localUser });  
-
 }
